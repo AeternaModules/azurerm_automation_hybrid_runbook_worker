1a79fd4758597c8f8fb8916407dbcccca72c78c4
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:55:28 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "automation_hybrid_runbook_workers" {
+  description = "All automation_hybrid_runbook_worker resources"
+  value       = azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers
+}
+
