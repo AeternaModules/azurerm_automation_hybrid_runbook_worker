36a5e9b7be34ae6311cb9375a92a48b9aaa19a45
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,44 @@ output "automation_hybrid_runbook_workers" {
   description = "All automation_hybrid_runbook_worker resources"
   value       = azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers
 }
+output "automation_hybrid_runbook_workers_automation_account_name" {
+  description = "List of automation_account_name values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.automation_account_name]
+}
+output "automation_hybrid_runbook_workers_ip" {
+  description = "List of ip values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.ip]
+}
+output "automation_hybrid_runbook_workers_last_seen_date_time" {
+  description = "List of last_seen_date_time values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.last_seen_date_time]
+}
+output "automation_hybrid_runbook_workers_registration_date_time" {
+  description = "List of registration_date_time values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.registration_date_time]
+}
+output "automation_hybrid_runbook_workers_resource_group_name" {
+  description = "List of resource_group_name values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.resource_group_name]
+}
+output "automation_hybrid_runbook_workers_vm_resource_id" {
+  description = "List of vm_resource_id values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.vm_resource_id]
+}
+output "automation_hybrid_runbook_workers_worker_group_name" {
+  description = "List of worker_group_name values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.worker_group_name]
+}
+output "automation_hybrid_runbook_workers_worker_id" {
+  description = "List of worker_id values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.worker_id]
+}
+output "automation_hybrid_runbook_workers_worker_name" {
+  description = "List of worker_name values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.worker_name]
+}
+output "automation_hybrid_runbook_workers_worker_type" {
+  description = "List of worker_type values across all automation_hybrid_runbook_workers"
+  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.worker_type]
+}
 
