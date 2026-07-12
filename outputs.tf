aa5ec020626c7589cfd19061e46f38bd8347eee7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "automation_hybrid_runbook_workers" {
-  description = "All automation_hybrid_runbook_worker resources"
-  value       = azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers
-}
 output "automation_hybrid_runbook_workers_automation_account_name" {
-  description = "List of automation_account_name values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.automation_account_name }
 }
 output "automation_hybrid_runbook_workers_ip" {
-  description = "List of ip values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.ip]
+  description = "Map of ip values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.ip }
 }
 output "automation_hybrid_runbook_workers_last_seen_date_time" {
-  description = "List of last_seen_date_time values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.last_seen_date_time]
+  description = "Map of last_seen_date_time values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.last_seen_date_time }
 }
 output "automation_hybrid_runbook_workers_registration_date_time" {
-  description = "List of registration_date_time values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.registration_date_time]
+  description = "Map of registration_date_time values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.registration_date_time }
 }
 output "automation_hybrid_runbook_workers_resource_group_name" {
-  description = "List of resource_group_name values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.resource_group_name }
 }
 output "automation_hybrid_runbook_workers_vm_resource_id" {
-  description = "List of vm_resource_id values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.vm_resource_id]
+  description = "Map of vm_resource_id values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.vm_resource_id }
 }
 output "automation_hybrid_runbook_workers_worker_group_name" {
-  description = "List of worker_group_name values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.worker_group_name]
+  description = "Map of worker_group_name values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.worker_group_name }
 }
 output "automation_hybrid_runbook_workers_worker_id" {
-  description = "List of worker_id values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.worker_id]
+  description = "Map of worker_id values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.worker_id }
 }
 output "automation_hybrid_runbook_workers_worker_name" {
-  description = "List of worker_name values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.worker_name]
+  description = "Map of worker_name values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.worker_name }
 }
 output "automation_hybrid_runbook_workers_worker_type" {
-  description = "List of worker_type values across all automation_hybrid_runbook_workers"
-  value       = [for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : v.worker_type]
+  description = "Map of worker_type values across all automation_hybrid_runbook_workers, keyed the same as var.automation_hybrid_runbook_workers"
+  value       = { for k, v in azurerm_automation_hybrid_runbook_worker.automation_hybrid_runbook_workers : k => v.worker_type }
 }
 
